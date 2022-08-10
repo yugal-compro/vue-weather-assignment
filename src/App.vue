d38004866f57ff7f66a292ce4c86758293b79c48
Yugal Vashishth <yugal.vashishth@comprotechnologies.com>
2022-08-10 21:43:20 +0530

    improved ui of app

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,5 @@ export default {
 };
 </script>
 
-<style lang="scss">
-@import "~bootstrap/scss/functions";
-@import "~bootstrap/scss/variables";
-@import "~bootstrap/scss/mixins";
-body{
-    height: 100vh;
-    padding: 0;
-    margin: 0;
-    @include media-breakpoint-up(md) {
-      background-image: url('../src/assets/background-images/bg.webp');
-    }
-    background-image: url('https://thumbs.dreamstime.com/b/blue-winter-mobile-phone-wallpaper-rich-saturated-bright-color-tinted-grainy-vertical-background-dark-light-tones-surface-227498195.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    backdrop-filter: blur(10px);
-}
+<style lang="scss" src="./assets/style.scss">
 </style>
--- src/views/home-page/home-page.vue
+++ src/views/home-page/home-page.vue
@@ -5,13 +5,6 @@
 import { mapGetters } from 'vuex';
 import WeatherInfo from '../../components/weather-info/weather-info.vue';
 import SearchCity from '../../components/search-city/search-city.vue';
-import cloudBg from '../../assets/background-images/clouds-bg.jpg';
-import snowBg from '../../assets/background-images/snow-bg.jpg';
-import rainBg from '../../assets/background-images/rain-bg.jpg';
-import hazeBg from '../../assets/background-images/haze-bg.jpg';
-import weatherBg from '../../assets/background-images/weather-bg.jpg';
-import thunderstormBg from '../../assets/background-images/thunderstorm-bg.jpg';
-import clearSkyBg from '../../assets/background-images/clearsky-bg.jpg';
 
 export default {
   name: 'HomePage',
@@ -19,39 +12,16 @@ export default {
     WeatherInfo,
     SearchCity
   },
+  computed: {
+    ...mapGetters('weatherData', {
+      weather: 'getWeatherData'
+    }),
+    weatherMain() {
+      if (this.weather == null) return 'Haze';
+      return this.weather.weather[0].main;
+    }
+  },
   methods: {
-    getImage() {
-      // const mediaQuery = window.matchMedia('(max-width: 768px)');
-      // if (mediaQuery.matches) {
-      //   return 'https://thumbs.dreamstime.com/b/blue-winter-mobile-phone-wallpaper-rich-saturated-bright-color-tinted-grainy-vertical-background-dark-light-tones-surface-227498195.jpg';
-      // }
-      if (this.weather == null) {
-        return weatherBg;
-      }
-      const weather = this.weather.weather[0].main;
-      if (weather === 'Clouds') {
-        return cloudBg;
-      }
-      if (weather === 'Rain') {
-        return rainBg;
-      }
-      if (weather === 'Thunderstorm') {
-        return thunderstormBg;
-      }
-      if (weather === 'Drizzle') {
-        return rainBg;
-      }
-      if (weather === 'Snow') {
-        return snowBg;
-      }
-      if (weather === 'Clear') {
-        return clearSkyBg;
-      }
-      if (weather === 'Haze' || weather === 'Mist') {
-        return hazeBg;
-      }
-      return weatherBg;
-    },
     showPosition(position) {
       this.latitude = position.coords.latitude;
       this.longitude = position.coords.longitude;
@@ -69,11 +39,6 @@ export default {
         this.$store.dispatch('weatherData/fetchUserLocation');
       }
     }
-  },
-  computed: {
-    ...mapGetters('weatherData', {
-      weather: 'getWeatherData'
-    })
   },
   created() {
     this.$store.dispatch('weatherData/fetchCitiesData');
